dc16d574ee4af6590ae4127a1e09a7cc63b33c04
xuytxuyt <xyt5517@163.com>
2024-04-10 15:16:11 +0800

    20240410

--- patch_test.jl
+++ patch_test.jl
@@ -12,28 +12,28 @@ E = 3E6;
 ν = 0.3;
 h = 1
 Dᵇ = E*h^3/12/(1-ν^2)
+Dˢ = 5/6*E*h/(2*(1+ν))
 
-n=2
-w(x,y) = (x+y)^n
-w₁(x,y) = n*(x+y)^abs(n-1)
-w₂(x,y) = n*(x+y)^abs(n-1)
+n =3
+w(x,y) = -Dᵇ/Dˢ*12*x-Dᵇ/Dˢ*12*y+(x+y)^n
+w₁(x,y) = -Dᵇ/Dˢ*12+n*(x+y)^abs(n-1)
+w₂(x,y) = -Dᵇ/Dˢ*12+n*(x+y)^abs(n-1)
 w₁₁(x,y) = n*(n-1)*(x+y)^abs(n-2)
 w₂₂(x,y) = n*(n-1)*(x+y)^abs(n-2)
-
-θ₁(x,y) = 2*(x+y)^abs(n-1)
-θ₂(x,y) = 2*(x+y)^abs(n-1)
-θ₁₁(x,y) = 2*(n-1)*(x+y)^abs(n-2)
-θ₁₂(x,y) = 2*(n-1)*(x+y)^abs(n-2)
-θ₂₂(x,y) = 2*(n-1)*(x+y)^abs(n-2)
+θ₁(x,y) = n*(x+y)^abs(n-1)
+θ₂(x,y) = n*(x+y)^abs(n-1)
+θ₁₁(x,y) = n*(n-1)*(x+y)^abs(n-2)
+θ₁₂(x,y) = n*(n-1)*(x+y)^abs(n-2)
+θ₂₂(x,y) = n*(n-1)*(x+y)^abs(n-2)
 
 M₁₁(x,y)= -Dᵇ*(θ₁₁(x,y)+ν*θ₂₂(x,y))
 M₁₂(x,y)= -Dᵇ*(1-ν)*θ₁₂(x,y)
 M₂₂(x,y)= -Dᵇ*(ν*θ₁₁(x,y)+θ₂₂(x,y))
 
-# Q₁(x,y) = E*h/(2*(1+ν))*(w₁(x,y)-θ₁(x,y))
-# Q₂(x,y) = E*h/(2*(1+ν))*(w₂(x,y)-θ₂(x,y))
-# Q₁₁(x,y) = E*h/(2*(1+ν))*(w₁₁(x,y)-θ₁₁(x,y))
-# Q₂₂(x,y) = E*h/(2*(1+ν))*(w₂₂(x,y)-θ₂₂(x,y))
+Q₁(x,y) = Dˢ*(w₁(x,y)-θ₁(x,y))
+Q₂(x,y) = Dˢ*(w₂(x,y)-θ₂(x,y))
+Q₁₁(x,y) = Dˢ*(w₁₁(x,y)-θ₁₁(x,y))
+Q₂₂(x,y) = Dˢ*(w₂₂(x,y)-θ₂₂(x,y))
 
 eval(prescribeForFem)
 
@@ -45,7 +45,9 @@ set𝝭!(elements["Γ₃"])
 set𝝭!(elements["Γ₄"])
 
 ops = [
-    Operator{:∫κᵢⱼγᵢⱼdΩ}(:E=>E,:ν=>ν,:h=>h),
+    Operator{:∫κMγQdΩ}(:E=>E,:ν=>ν,:h=>h),
+    Operator{:∫κMdΩ}(:E=>E,:ν=>ν,:h=>h),
+    Operator{:∫γQdΩ}(:E=>E,:ν=>ν,:h=>h),
     Operator{:∫wqdΩ}(),
     Operator{:∫vwdΓ}(:α=>1e8*E),
     Operator{:∫vθdΓ}(:α=>1e8*E),
@@ -56,27 +58,33 @@ ops = [
 
 ]
 k = zeros(3*nₚ,3*nₚ)
+kᵇ = zeros(3*nₚ,3*nₚ)
+kˢ = zeros(3*nₚ,3*nₚ)
 f = zeros(3*nₚ)
+ops[2](elements["Ω"],kᵇ)
+ops[3](elements["Ω"],kˢ)
+ops[4](elements["Ω"],f)
+ops[5](elements["Γ₁"],k,f)
+ops[5](elements["Γ₂"],k,f)
+ops[5](elements["Γ₃"],k,f)
+ops[5](elements["Γ₄"],k,f)
+ops[6](elements["Γ₁"],k,f)
+ops[6](elements["Γ₂"],k,f)
+ops[6](elements["Γ₃"],k,f)
+ops[6](elements["Γ₄"],k,f)
+ops[7](elements["Γ₁"],f)
+ops[7](elements["Γ₂"],f)
+ops[7](elements["Γ₃"],f)
+ops[7](elements["Γ₄"],f)
+ops[8](elements["Γ₁"],f)
+ops[8](elements["Γ₂"],f)
+ops[8](elements["Γ₃"],f)
+ops[8](elements["Γ₄"],f)
 
-ops[1](elements["Ω"],k)
-ops[3](elements["Γ₁"],k,f)
-ops[3](elements["Γ₂"],k,f)
-ops[3](elements["Γ₃"],k,f)
-ops[3](elements["Γ₄"],k,f)
-ops[4](elements["Γ₁"],k,f)
-ops[4](elements["Γ₂"],k,f)
-ops[4](elements["Γ₃"],k,f)
-ops[4](elements["Γ₄"],k,f)
-
-# ops[6](elements["Γ₁"],f)
-# ops[6](elements["Γ₂"],f)
-# ops[6](elements["Γ₃"],f)
-# ops[6](elements["Γ₄"],f)
-
-d= k\f
+d = (kᵇ+kˢ+k)\f
 d₁ = d[1:3:3*nₚ]
 d₂ = d[2:3:3*nₚ]
 d₃ = d[3:3:3*nₚ]
 
 push!(nodes,:d=>d₂)
-L₂ = ops[7](elements["Ω"])
+L₂ = ops[9](elements["Ω"])
