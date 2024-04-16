947588a607e6f46315d77fc1edb3265f46821c96
WuJC <jcwu@hqu.edu.cn>
2024-04-16 16:12:04 +0800

    temp

--- patch_test.jl
+++ patch_test.jl
@@ -1,36 +1,37 @@
-using ApproxOperator, JLD,XLSX
+using ApproxOperator, JLD
 
 import BenchmarkExample: BenchmarkExample
 
 include("import_patch_test.jl")
-ndiv = 3
+ndiv = 32
 elements, nodes = import_patch_test_fem("msh/patchtest_"*string(ndiv)*".msh");
 
 nₚ = length(nodes)
 
-E = 3E6;
+E = 1;
 ν = 0.3;
 h = 1
 Dᵇ = E*h^3/12/(1-ν^2)
 Dˢ = 5/6*E*h/(2*(1+ν))
 
-n =2
+n = 2
 w(x,y) = (x+y)^n
 w₁(x,y) = n*(x+y)^abs(n-1)
 w₂(x,y) = n*(x+y)^abs(n-1)
 w₁₁(x,y) = n*(n-1)*(x+y)^abs(n-2)
 w₂₂(x,y) = n*(n-1)*(x+y)^abs(n-2)
-θ₁(x,y) = 1.0
-θ₂(x,y) = 1.0
-θ₁₁(x,y)  = 0.0
-θ₁₂(x,y)  = 0.0
-θ₂₂(x,y)  = 0.0
-θ₁₁₁(x,y) = 0.0
-θ₁₁₂(x,y) = 0.0
-θ₁₂₂(x,y) = 0.0
-θ₁₂₁(x,y) = 0.0
-θ₂₂₂(x,y) = 0.0
-θ₂₂₁(x,y) = 0.0
+m = 2
+θ₁(x,y) = (x+y)^m
+θ₂(x,y) = (x+y)^m
+θ₁₁(x,y)  = m*(x+y)^abs(m-1)
+θ₁₂(x,y)  = m*(x+y)^abs(m-1)
+θ₂₂(x,y)  = m*(x+y)^abs(m-1)
+θ₁₁₁(x,y) = m*(m-1)*(x+y)^abs(m-2)
+θ₁₁₂(x,y) = m*(m-1)*(x+y)^abs(m-2)
+θ₁₂₂(x,y) = m*(m-1)*(x+y)^abs(m-2)
+θ₁₂₁(x,y) = m*(m-1)*(x+y)^abs(m-2)
+θ₂₂₂(x,y) = m*(m-1)*(x+y)^abs(m-2)
+θ₂₂₁(x,y) = m*(m-1)*(x+y)^abs(m-2)
 # θ₁(x,y) = n*(x+y)^abs(n-1)
 # θ₂(x,y) = n*(x+y)^abs(n-1)
 # θ₁₁(x,y) = n*(n-1)*(x+y)^abs(n-2)
@@ -98,30 +99,30 @@ ops[2](elements["Ω"],kᵇ)
 ops[3](elements["Ω"],kˢ)
 ops[4](elements["Ω"],f)
 ops[5](elements["Ω"],f)
-ops[6](elements["Γ₁"],k,f)
-ops[6](elements["Γ₂"],k,f)
-ops[6](elements["Γ₃"],k,f)
-ops[6](elements["Γ₄"],k,f)
-ops[7](elements["Γ₁"],k,f)
-ops[7](elements["Γ₂"],k,f)
-ops[7](elements["Γ₃"],k,f)
-ops[7](elements["Γ₄"],k,f)
-# ops[8](elements["Γ₁"],f)
-# ops[8](elements["Γ₂"],f)
-# ops[8](elements["Γ₃"],f)
-# ops[8](elements["Γ₄"],f)
+# ops[6](elements["Γ₁"],k,f)
+# ops[6](elements["Γ₂"],k,f)
+# ops[6](elements["Γ₃"],k,f)
+# ops[6](elements["Γ₄"],k,f)
+# ops[7](elements["Γ₁"],k,f)
+# ops[7](elements["Γ₂"],k,f)
+# ops[7](elements["Γ₃"],k,f)
+# ops[7](elements["Γ₄"],k,f)
+ops[8](elements["Γ₁"],f)
+ops[8](elements["Γ₂"],f)
+ops[8](elements["Γ₃"],f)
+ops[8](elements["Γ₄"],f)
 # ops[9](elements["Γ₁"],f)
 # ops[9](elements["Γ₂"],f)
 # ops[9](elements["Γ₃"],f)
 # ops[9](elements["Γ₄"],f)
 
-d = (kᵇ+kˢ+k)\f
-d₁ = d[1:3:3*nₚ]
-d₂ = d[2:3:3*nₚ]
-d₃ = d[3:3:3*nₚ]
+# d = (kᵇ+kˢ+k)\f
+# d₁ = d[1:3:3*nₚ]
+# d₂ = d[2:3:3*nₚ]
+# d₃ = d[3:3:3*nₚ]
 
-push!(nodes,:d=>d₁)
-L₂ = ops[10](elements["Ω"])
+# push!(nodes,:d=>d₁)
+# L₂ = ops[10](elements["Ω"])
 # index = [8,16,32,64]
 # XLSX.openxlsx("./xlsx/patch_test.xlsx", mode="rw") do xf
 #     Sheet = xf[2]
@@ -130,3 +131,19 @@ L₂ = ops[10](elements["Ω"])
 #     Sheet["C"*string(ind)] = L₂
 #     Sheet["D"*string(ind)] = log10(L₂)
 # end
+
+d = zeros(3*nₚ)
+for (i,node) in enumerate(nodes)
+    x = node.x
+    y = node.y
+    z = node.z
+
+    d[3*i-2] = w(x,y)
+    d[3*i-1] = θ₁(x,y)
+    d[3*i] = θ₂(x,y)
+end
+
+# k
+# kᵇ*d - f
+kˢ*d - f
+# err1 = kᵇ*d
